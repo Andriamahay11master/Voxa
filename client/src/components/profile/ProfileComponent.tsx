703bbd78117cc2af3f8eec45b942bee8f0a21a3e
Mahay <hirimanana@yahoo.fr>
2025-04-29 14:41:06 +0400

    add state edit on component Profile + cancel action

--- client/src/components/profile/ProfileComponent.tsx
+++ client/src/components/profile/ProfileComponent.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import { UserType } from "../../models/UserType";
 
 interface ProfileComponentProps {
   user: UserType;
 }
 const ProfileComponent = ({ user }: ProfileComponentProps) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const handleCancel = () => {
+    setEditMode(false);
+  };
   return (
     <div className="profile-component">
+      <button className="btn btn-icon" onClick={() => setEditMode(true)}>
+        <i className="icon-edit"></i>
+      </button>
       <div className="profil-top">
         <div className="profil-picture">
           <img
@@ -32,7 +41,15 @@ const ProfileComponent = ({ user }: ProfileComponentProps) => {
           </div>
           <div className="profil-item-right">
             <h3 className="profil-info-title">Name</h3>
-            <p>{user.displayName}</p>
+            {editMode ? (
+              <input
+                type="text"
+                value={user.displayName}
+                onChange={(e) => console.log(e.target.value)}
+              />
+            ) : (
+              <p>{user.displayName}</p>
+            )}
           </div>
         </div>
         <div className="profil-info-item">
@@ -41,7 +58,15 @@ const ProfileComponent = ({ user }: ProfileComponentProps) => {
           </div>
           <div className="profil-item-right">
             <h3 className="profil-info-title">Infos</h3>
-            <p>{user.bio || "Hello, I'm using Voxa"}</p>
+            {editMode ? (
+              <input
+                type="text"
+                value={user.bio}
+                onChange={(e) => console.log(e.target.value)}
+              />
+            ) : (
+              <p>{user.bio || "Hello, I'm using Voxa"}</p>
+            )}
           </div>
         </div>
         <div className="profil-info-item">
@@ -50,10 +75,32 @@ const ProfileComponent = ({ user }: ProfileComponentProps) => {
           </div>
           <div className="profil-item-right">
             <h3 className="profil-info-title">Telephone number</h3>
-            <p>{user.phoneNumber || "Not set"}</p>
+
+            {editMode ? (
+              <input
+                type="text"
+                value={user.phoneNumber}
+                onChange={(e) => console.log(e.target.value)}
+              />
+            ) : (
+              <p>{user.phoneNumber || "Not set"}</p>
+            )}
           </div>
         </div>
       </div>
+      {editMode && (
+        <div className="profil-edit-action">
+          <button
+            className="btn btn-primary"
+            onClick={() => setEditMode(false)}
+          >
+            Save
+          </button>
+          <button className="btn btn-cancel" onClick={handleCancel}>
+            Cancel
+          </button>
+        </div>
+      )}
     </div>
   );
 };

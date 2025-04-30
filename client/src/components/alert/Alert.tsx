a3cf779e2602490b09bbc5cc7ed22156592d652d
Mahay <hirimanana@yahoo.fr>
2025-04-30 20:12:26 +0400

    styling component alert

--- client/src/components/alert/Alert.tsx
+++ client/src/components/alert/Alert.tsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
+
 interface AlertProps {
   text: string;
   type: string;
 }
 const Alert = ({ text, type }: AlertProps) => {
+  const [showAlert, setShowAlert] = useState(true);
+  const closeAlert = () => {
+    setShowAlert(false);
+  };
+
+  if (!showAlert) return null;
   return (
     <div className={"alert-component " + type}>
-      <button type="button" className="btn btn-icon">
-        <i className="icon-close"></i>
+      <button type="button" className="btn btn-icon" onClick={closeAlert}>
+        <i className="icon-clear"></i>
       </button>
       <div className="alert-content">
         <p>{text}</p>
